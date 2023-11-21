1a9a182df37011e84c8283f9ad1a8415cd6f2ba7
itd <itd@a.a>
2023-11-21 10:15:55 +0100

    adf

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const gamearea=document.querySelector("#gamearea")
+const gamearea = document.querySelector('#gamearea')
 
-for(let i=0;i<12;i++)
-{
-    const szamdoboz=document.createElement("div")
-    szamdoboz.innerHTML="A"
-    gamearea.appendChild(szamdoboz)
+for (let i = 0; i < 12; i++) {
+  const szamdoboz = document.createElement('div')
+  let x = Math.floor(Math.random() * 12) + 1
+  szamdoboz.innerHTML = x
+  gamearea.appendChild(szamdoboz)
+  szamdoboz.addEventListener('click', () => {
+    szamdoboz.innerHTML = ''
+  })
 }
